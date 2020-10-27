6733ad822c4c70c9bc1c2d42f55c6fa52a587fe6
Giovanni Severo <giosvro@gmail.com>
2020-10-27 18:37:21 -0300

    Changed Piece

--- Chess/src/Model/Piece.java
+++ Chess/src/Model/Piece.java
@@ -1,18 +1,14 @@
 package Model;
 
 public abstract class Piece {
-	Position position;
 	boolean isAlive;
 	Color color;
-	PieceType type;
 	
 	Piece() {
 		
 	}
 	
-	public void move(Position newPosition) {
-		this.position = newPosition;
-	}
+	public void move(Position newPosition) { }
 	
 	public void die() {
 		this.isAlive = true;
