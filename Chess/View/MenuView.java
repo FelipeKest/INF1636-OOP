059c7d5e7e0e74bfd78d17bb182f8c1f6d434938
Felipe Kestelman <fpkest@gmail.com>
2020-12-12 22:16:17 -0300

    StartGame View implemented

--- Chess/View/MenuView.java
+++ Chess/View/MenuView.java
@@ -9,39 +9,73 @@ import java.awt.event.*;
 public class MenuView extends JFrame implements ActionListener {
 	
 	
-	private JLabel titleL;
+	private JLabel titleL, nameL1, nameL2;
     private JButton startB, loadB, exitB;
+    private JTextField nameTF1, nameTF2;
 
     static JFrame frame1 = new JFrame();
 
     public MenuView() {
-        frame1.setSize(500,250);
+    	setupView();
+    }
+
+    private void setupView() {
+        frame1.setSize(500,450);
 
         Container mainP = frame1.getContentPane();
         mainP.setLayout(null);
 
-        titleL = new JLabel("WELCOME");
+        titleL = new JLabel("Xadrez");
+        nameL1 = new JLabel("Jogador 1");
+        nameL2 = new JLabel("Jogador 2"); 
+        nameTF1 = new JTextField();
+        nameTF2 = new JTextField();
         startB = new JButton("Iniciar");
         loadB = new JButton("Carregar jogo");
         exitB = new JButton("Sair");
-
-
+        
+        int currentHeight = 30;
+        
         mainP.add(titleL);
         titleL.setFont(new Font("Arial",Font.BOLD,20));
-        titleL.setBounds(100, 30, 200, 50);
+        titleL.setBounds(200, currentHeight, 200, 50);
 
+        currentHeight+=60;
+        
+        mainP.add(nameL1);
+        nameL1.setFont(new Font("Arial",Font.BOLD,15));
+        nameL1.setBounds(200, currentHeight, 100, 15);
+      
+        currentHeight+=20;
+        
+        mainP.add(nameTF1);
+        nameTF1.setBounds(200, currentHeight, 100, 20);
+        
+        currentHeight+=30;
+        
+        mainP.add(nameL2);
+        nameL2.setFont(new Font("Arial",Font.BOLD,15));
+        nameL2.setBounds(200, currentHeight, 100, 15);
+        
+        currentHeight+=20;
+
+        mainP.add(nameTF2);
+        nameTF2.setBounds(200, currentHeight, 100, 20);
+
+        currentHeight+=30;
+        
         mainP.add(startB);
-        startB.setMnemonic(KeyEvent.VK_S);
-        startB.setBounds(200, 80, 100, 20);
+        startB.setBounds(200, currentHeight, 100, 20);
 
+        currentHeight+=30;
+        
         mainP.add(loadB);
-        loadB.setMnemonic(KeyEvent.VK_H);
-        loadB.setBounds(150, 100, 200, 20);
+        loadB.setBounds(150, currentHeight, 200, 20);
 
+        currentHeight+=30;
 
         mainP.add(exitB);
-        exitB.setMnemonic(KeyEvent.VK_E);
-        exitB.setBounds(200, 120, 100, 20);
+        exitB.setBounds(200, currentHeight, 100, 20);
 
 
         startB.addActionListener(this);
@@ -50,11 +84,7 @@ public class MenuView extends JFrame implements ActionListener {
 
         frame1.setVisible(true);
         frame1.setResizable(false);
-
-        System.out.println("dwada");
     }
-
-
 
     public static void main(String[]args)
     {
